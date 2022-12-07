0f2fc5fe19e4b9976c19dd168ab0dd8af11fbfc6
Sanjay Varyani <sanjayvaryani1@gmail.com>
2022-12-07 10:50:31 +0530

    Task3 updated

--- Task3/Task3a_Optimal_Subsets/lib/task3a_optimal_subsets.ex
+++ Task3/Task3a_Optimal_Subsets/lib/task3a_optimal_subsets.ex
@@ -28,10 +28,26 @@ defmodule Task3aOptimalSubsets do
   #Details:
         Finds the valid sum values from the given 2d matrix
   """
+
+  #  Configuration of char list should be printed as list only
+  #  Source https://stackoverflow.com/questions/30037914/elixir-lists-interpreted-as-char-lists
+  IEx.configure(inspect: [charlists: :as_lists])
+
+  # #Example:
+  #            matrix_of_sum = [
+  #                              [21 ,"na", "na", "na", 12],
+  #                              ["na", "na", 12, "na", "na"],
+  #                              ["na", "na", "na", "na", "na"],
+  #                              [17, "na", "na", "na", "na"],
+  #                              ["na", 22, "na", "na", "na"]
+  #                            ]
+
+
   def valid_sum(matrix_of_sum \\ random_matrix_generator()) do
     IO.inspect matrix_of_sum, label:  "matrix_of_sum"
     ### Write your code here ###
-
+    matrix_of_sum |> List.flatten |>
+         Enum.filter(fn x -> if is_integer(x), do: x end)
   end
 
 
@@ -47,13 +63,35 @@ defmodule Task3aOptimalSubsets do
         Finds the all possible subsets from given array of digits for a 2 digit value
   """
 
+  # array_of_digits = [3, 5, 2, 7, 4, 2, 3]
+  #              and sum_val = 10
+
+  def combination(list,num)
+  def combination(_list, 0), do: [[]]
+  def combination(list=[], _num), do: list
+  def combination([head | tail],num) do
+   Enum.map(combination(tail,num - 1),&[head|&1])++
+   combination(tail,num)
+  end
+
   def sum_of_one(array_of_digits \\ random_list_generator(), sum_val \\ Enum.random(11..20)) do
     IO.inspect array_of_digits, label:  "array_of_digits"
     IO.inspect sum_val, label:  "sum_val"
     ### Write your code here ###
-
+    max_count = Enum.count(array_of_digits)
+    loop_1(array_of_digits,1,max_count,sum_val,[])
   end
-
+  def loop_1(array,st,en,sum,result) when st <= en do
+   result = result ++ Enum.filter(combination(array,st),&(if Enum.sum(&1) == sum, do: Enum.sum(&1)))
+   loop_1(array,st+1,en,sum,result)
+  end
+  def loop_1(_array,st,en,_sum,result) when st > en do
+   result
+  end
+  def sum_of_one(_array_of_digits, sum_val) when sum_val == 0 do
+    result = []
+    result
+  end
   @doc """
   #Function name:
         sum_of_all
@@ -65,12 +103,37 @@ defmodule Task3aOptimalSubsets do
   #Details:
         Finds the all possible subsets from given array of digits for all valid sums elements of given 2d matrix
   """
-  def sum_of_all(array_of_digits \\ random_list_generator(), matrix_of_sum \\ random_matrix_generator()) do
+
+  # #Example call:
+  #          if given array of digits is as follows:
+  #            array_of_digits = [3, 5, 2, 7, 4, 2, 3]
+  #          and if given 2d matrix is as follows:
+  #            matrix_of_sum = [
+  #                              [21 ,"na", "na", "na", 12],
+  #                              ["na", "na", 12, "na", "na"],
+  #                              ["na", "na", "na", "na", "na"],
+  #                              [17, "na", "na", "na", "na"],
+  #                              ["na", 22, "na", "na", "na"]
+  #                            ]
+
+   def sum_of_all(array_of_digits \\ random_list_generator(), matrix_of_sum \\ random_matrix_generator()) do
     IO.inspect array_of_digits, label:  "array_of_digits"
     IO.inspect matrix_of_sum, label:  "matrix_of_sum"
     ### Write your code here ###
+    list_of_sum = valid_sum(matrix_of_sum)
+		loop_2(array_of_digits, list_of_sum, 0, Enum.count(list_of_sum), %{})
 
-  end
+   end
+
+   def loop_2(array_of_digits,list_of_sum,st,en,result) when st < en do
+		list = sum_of_one(array_of_digits,Enum.at(list_of_sum,st))
+		IO.inspect array_of_digits
+		result = Map.merge(result,%{Enum.at(list_of_sum,st) => list})
+		loop_2(array_of_digits,list_of_sum,st+1,en,result)
+	 end
+	 def loop_2(_array_of_digits,_list_of_sum,st,en,result) when st == en do
+		result
+	 end
 
   @doc """
   #Function name:
